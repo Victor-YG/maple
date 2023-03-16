4bab7da8beb9d6a81abb460ca6c0920c3ca0b960
leohanxc <leo.xc.han@protonmail.com>
2023-03-15 22:09:51 -0400

    update run_test.sh for verilator

--- run_test.sh
+++ run_test.sh
@@ -8,6 +8,6 @@ fi
 # Running Other tests
 tests=("dma" "dcp_uni" "contiguous_allocation" "custom_acc")
 name=${tests[$1]}
-sims -sys=manycore -ariane -decoupling -vcs_run ${name}.c -x_tiles=1 -y_tiles=1 -finish_mask=0x001 -gcc_args=-DBARE_METAL -rtl_timeout=1000000 -asm_diag_root=../maple
+sims -sys=manycore -ariane -decoupling -vlt_run ${name}.c -x_tiles=1 -y_tiles=1 -finish_mask=0x001 -gcc_args=-DBARE_METAL -rtl_timeout=1000000 -asm_diag_root=../maple
 cp fake_uart.log res/${name}.txt
 cp sims.log res/${name}_sims.txt
