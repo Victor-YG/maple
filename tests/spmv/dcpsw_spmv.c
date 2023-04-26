8c6154ee817b32c5b88eeb5dcd36900192614e54
leohanxc <leo.xc.han@protonmail.com>
2023-04-25 23:27:03 -0400

    updated dcpsp and dcpsw spmv

--- tests/spmv/dcpsw_spmv.c
+++ tests/spmv/dcpsw_spmv.c
@@ -15,7 +15,6 @@
 #define RES 1
 #define FINE 1
 //#define DOUBLEP 1
-#define FIFO_SIZE 32
 
 #ifndef NUM_A
     #define NUM_A 1
@@ -32,6 +31,11 @@
     // If we have same amount of A and E, FIFO count is NUM_A
     #define NUM NUM_A
 #endif
+
+#define FIFO_SIZE 32
+
+#define ACCESS 0
+#define EXECUTE 1
 
 /***FIFO counters***/
 volatile static uint32_t prog0_produce_cnt;
@@ -65,12 +69,6 @@ volatile static uint8_t prog1_execute_done;
 /***PROG0 FUNCTIONS***/
 
 void prog0_access_init(uint32_t id) {
-    #ifdef MAP
-    for (int k = 0; k < NUM_E; k++){
-        dec_open_producer(id+k*NUM_A);
-        dec_set_base64(id+k*NUM_A,x);
-    }
-    #else
     dec_open_producer(id);
     //Set the Base pointer, without it we would need to push &x[idx[k]]
     dec_set_base64(id,x);
@@ -79,15 +77,10 @@ void prog0_access_init(uint32_t id) {
     k_prog0_a = 0;
     prog0_produce_cnt = 0;
     prog0_access_done = 0;
-    #endif
 }
 
 void prog0_execute_init(uint32_t exec_id) {
-    #ifdef MAP
-    for (int k = 0; k < NUM_A; k++) dec_open_consumer(exec_id+NUM_E*k);
-    #else
     dec_open_consumer(exec_id);
-    #endif
     i_prog0_e = exec_id;
     k_prog0_e = 0;
     yi0_prog0_e = 0;
@@ -96,28 +89,22 @@ void prog0_execute_init(uint32_t exec_id) {
 }
 
 void prog0_access_kernel(uint32_t id, uint32_t threshold) {
-    int i = i_prog0_a;
-    int k = k_prog0_a;
+    uint32_t i = i_prog0_a;
+    uint32_t k = k_prog0_a;
     uint8_t stop_switching = prog1_execute_done;
     uint32_t produce_cnt = 0;
     uint32_t produce_threshold = threshold;
     // LK;printf("prog0_access, i: %d\n", i);ULK;
-    while (i < R) {
+    for (; i < R; i += (2 * NUM)) {
         // LK;printf("Producer ID: %d, row: %d, threshold: %d\n", id, i, threshold);ULK;
-        #ifdef MAP
-        uint32_t fif = i%NUM;
-        #else
-        uint32_t fif = id;
-        #endif
 
-        int end = ptr[i+1];
-        int endm1 = end-1;
+        uint32_t end = ptr[i+1];
 
         if (k == 0) {
             k = ptr[i];
         }
         for (; k < end; k++){
-            if ((produce_cnt >= produce_threshold) && (!stop_switching)) {
+            if (((produce_cnt + 1) > produce_threshold) && (!stop_switching)) {
                 stop_switching = prog1_execute_done;
                 if (!stop_switching) {
                     prog0_produce_cnt += produce_cnt;
@@ -125,27 +112,24 @@ void prog0_access_kernel(uint32_t id, uint32_t threshold) {
                     i_prog0_a = i;
                     k_prog0_a = k;
                     produce_threshold = FIFO_SIZE - (prog0_produce_cnt - prog0_consume_cnt);
-                    if (produce_threshold <= (FIFO_SIZE/2)) {
+                    if (produce_threshold < 1) {
                         return;
                     }
                 }
             }
-            dec_load64_asynci_llc(fif,idx[k]);
+            dec_load64_asynci_llc(id,idx[k]);
             produce_cnt++;
         }
-        i += (2 * NUM);
         k = 0;
     }
 
     prog0_access_done = 1;
     // LK;printf("prog0 access done\n");ULK;
-    i_prog0_a = i;
-    k_prog0_a = 0;
     prog0_produce_cnt += produce_cnt;
 }
 
 void prog0_execute_kernel(uint32_t exec_id, uint32_t threshold) {
-    int i = i_prog0_e;
+    uint32_t i = i_prog0_e;
     uint8_t stop_switching = prog1_access_done;
     uint64_t yi0 = yi0_prog0_e;
     uint32_t k = k_prog0_e;
@@ -153,13 +137,8 @@ void prog0_execute_kernel(uint32_t exec_id, uint32_t threshold) {
     uint32_t consume_threshold = threshold;
     // LK;printf("prog0_execute, i: %d\n", i);ULK;
 
-    while (i < R){
+    for (; i < R; i += (2 * NUM)) {
         // LK;printf("Consumer ID: %d, row: %d\n", exec_id, i);ULK;
-        #ifdef MAP
-        uint32_t fifo = i%NUM;
-        #else
-        uint32_t fifo = exec_id;
-        #endif
 
         uint32_t start = ptr[i];
         uint32_t end = ptr[i+1];
@@ -167,9 +146,8 @@ void prog0_execute_kernel(uint32_t exec_id, uint32_t threshold) {
         if (k == 0) {
             k = start;
         }
-        uint64_t dat;
         for (; k < end; k++){
-            if ((consume_cnt >= consume_threshold) && (!stop_switching)) {
+            if (((consume_cnt + 1) > consume_threshold) && (!stop_switching)) {
                 stop_switching = prog1_access_done;
                 if (!stop_switching) {
                     prog0_consume_cnt += consume_cnt;
@@ -178,15 +156,13 @@ void prog0_execute_kernel(uint32_t exec_id, uint32_t threshold) {
                     i_prog0_e = i;
                     k_prog0_e = k;
                     consume_threshold = prog0_produce_cnt - prog0_consume_cnt;
-                    if (consume_threshold <= (FIFO_SIZE/2)) {
+                    if (consume_threshold < 1) {
                         return;
                     }
-                    // return;
                 }
             }
-            dat = dec_consume64(fifo);
+            uint64_t dat = dec_consume64(exec_id);
             consume_cnt++;
-            //dat = x[idx[k]];
             yi0 += val[k]*dat;
         }
         #ifdef RES
@@ -195,15 +171,12 @@ void prog0_execute_kernel(uint32_t exec_id, uint32_t threshold) {
             return;
         }
         #endif
-        i += (2 * NUM);
         k = 0;
         yi0 = 0;
     }
 
     prog0_execute_done = 1;
     // LK;printf("prog0 execute done\n");ULK;
-    i_prog0_e = i;
-    k_prog0_e = 0;
     prog0_consume_cnt += consume_cnt;
 }
 
@@ -219,12 +192,6 @@ void prog0_execute_finish(uint32_t exec_id) {
 /***PROG1 FUNCTIONS***/
 
 void prog1_access_init(uint32_t id) {
-    #ifdef MAP
-    for (int k = 0; k < NUM_E; k++){
-        dec_open_producer(id+k*NUM_A);
-        dec_set_base64(id+k*NUM_A,x);
-    }
-    #else
     dec_open_producer(id);
     //Set the Base pointer, without it we would need to push &x[idx[k]]
     dec_set_base64(id,x);
@@ -233,15 +200,10 @@ void prog1_access_init(uint32_t id) {
     k_prog1_a = 0;
     prog1_produce_cnt = 0;
     prog1_access_done = 0;
-    #endif
 }
 
 void prog1_execute_init(uint32_t exec_id) {
-    #ifdef MAP
-    for (int k = 0; k < NUM_A; k++) dec_open_consumer(exec_id+NUM_E*k);
-    #else
     dec_open_consumer(exec_id);
-    #endif
     i_prog1_e = exec_id;
     k_prog1_e = 0;
     yi0_prog1_e = 0;
@@ -250,28 +212,22 @@ void prog1_execute_init(uint32_t exec_id) {
 }
 
 void prog1_access_kernel(uint32_t id, uint32_t threshold) {
-    int i = i_prog1_a;
-    int k = k_prog1_a;
+    uint32_t i = i_prog1_a;
+    uint32_t k = k_prog1_a;
     uint8_t stop_switching = prog0_execute_done;
     uint32_t produce_cnt = 0;
     uint32_t produce_threshold = threshold;
     // LK;printf("prog1_access, i: %d\n",i);ULK;
-    while (i < R) {
+    for (; i < R; i += (2 * NUM)) {
         // LK;printf("Producer ID: %d, row: %d, threshold: %d\n", id, i, produce_threshold);ULK;
-        #ifdef MAP
-        uint32_t fif = i%NUM;
-        #else
-        uint32_t fif = id;
-        #endif
 
-        int end = ptr[i+1];
-        int endm1 = end-1;
+        uint32_t end = ptr[i+1];
 
         if (k == 0) {
             k = ptr[i];
         }
         for (; k < end; k++){
-            if ((produce_cnt >= produce_threshold && (!stop_switching))) {
+            if (((produce_cnt + 1) > produce_threshold && (!stop_switching))) {
                 stop_switching = prog0_execute_done;
                 if (!stop_switching) {
                     prog1_produce_cnt += produce_cnt;
@@ -279,53 +235,33 @@ void prog1_access_kernel(uint32_t id, uint32_t threshold) {
                     i_prog1_a = i;
                     k_prog1_a = k;
                     produce_threshold = FIFO_SIZE - (prog1_produce_cnt - prog1_consume_cnt);
-                    if (produce_threshold <= (FIFO_SIZE/2)) {
+                    if (produce_threshold < 1) {
                         return;
                     }
                     // return;
                 }
             }
-            #ifdef DOUBLEP
-            if (k!=endm1){
-                dec_load64_asynci(fif,((uint64_t)idx[k]) << 32 | ((uint64_t)idx[k+1]) );
-                produce_cnt++;
-                k++;
-            } else {
-            #endif
-                dec_load64_asynci_llc(fif,idx[k]);
-                produce_cnt++;
-                //dec_produce64(fif,x[idx[k]]);
-                //dec_prefetchi(fif, idx[k]);
-            #ifdef DOUBLEP
-            }
-            #endif
+            dec_load64_asynci_llc(id,idx[k]);
+            produce_cnt++;
         }
-        i += (2 * NUM);
         k = 0;
     }
 
     prog1_access_done = 1;
     // LK;printf("prog1 access done\n");ULK;
-    i_prog1_a = i;
-    k_prog1_a = 0;
     prog1_produce_cnt += produce_cnt;
 }
 
 void prog1_execute_kernel(uint32_t exec_id, uint32_t threshold) {
-    int i = i_prog1_e;
+    uint32_t i = i_prog1_e;
     uint8_t stop_switching = prog0_access_done;
-    uint32_t k = k_prog1_e;
     uint64_t yi0 = yi0_prog1_e;
+    uint32_t k = k_prog1_e;
     uint32_t consume_cnt = 0;
     uint32_t consume_threshold = threshold;
     // LK;printf("prog1_execute, i: %d\n",i);ULK;
-    while (i < R){
+    for (; i < R; i += (2 * NUM)) {
         // LK;printf("Consumer ID: %d, row: %d\n", exec_id, i);ULK;
-        #ifdef MAP
-        uint32_t fifo = i%NUM;
-        #else
-        uint32_t fifo = exec_id;
-        #endif
 
         uint32_t start = ptr[i];
         uint32_t end = ptr[i+1];
@@ -333,9 +269,8 @@ void prog1_execute_kernel(uint32_t exec_id, uint32_t threshold) {
         if (k == 0) {
             k = start;
         }
-        uint64_t dat;
         for (; k < end; k++){
-            if ((consume_cnt >= consume_threshold) && (!stop_switching)) {
+            if (((consume_cnt + 1) > consume_threshold) && (!stop_switching)) {
                 stop_switching = prog0_access_done;
                 if (!stop_switching) {
                     prog1_consume_cnt += consume_cnt;
@@ -344,15 +279,13 @@ void prog1_execute_kernel(uint32_t exec_id, uint32_t threshold) {
                     i_prog1_e = i;
                     k_prog1_e = k;
                     consume_threshold = prog1_produce_cnt - prog1_consume_cnt;
-                    if (consume_threshold <= (FIFO_SIZE/2)) {
+                    if (consume_threshold < 1) {
                         return;
                     }
-                    // return;
                 }
             }
-            dat = dec_consume64(fifo);
+            uint64_t dat = dec_consume64(exec_id);
             consume_cnt++;
-            //dat = x[idx[k]];
             yi0 += val[k]*dat;
         }
         #ifdef RES
@@ -361,15 +294,12 @@ void prog1_execute_kernel(uint32_t exec_id, uint32_t threshold) {
             return;
         }
         #endif
-        i += (2 * NUM);
         k = 0;
         yi0 = 0;
     }
 
     prog1_execute_done = 1;
     // LK;printf("prog1 execute done\n");ULK;
-    i_prog1_e = i;
-    k_prog1_e = 0;
     prog1_consume_cnt += consume_cnt;
 }
 
@@ -392,15 +322,18 @@ void _kernel_(uint32_t id, uint32_t core_num){
         // LK;printf("core: %d running\n", id);ULK;
 
         // Run
+        uint8_t role = ACCESS;
         while(!(prog0_access_done && prog1_execute_done)) {
             int consume_threshold = prog1_produce_cnt - prog1_consume_cnt;
             int produce_threshold = FIFO_SIZE - (prog0_produce_cnt - prog0_consume_cnt);
-            if ((((consume_threshold > ((FIFO_SIZE * 3)/4))) || prog0_access_done)) {
+            if (((role == EXECUTE) || prog0_access_done) && (!prog1_execute_done)) {
                 // LK;printf("exec: %d\n", exec_id);ULK;
                 prog1_execute_kernel(exec_id, consume_threshold);
+                role = ACCESS;
             } else {
                 // LK;printf("access: %d\n", id);ULK;
                 prog0_access_kernel(id, produce_threshold);
+                role = EXECUTE;
             }
         }
 
@@ -418,15 +351,18 @@ void _kernel_(uint32_t id, uint32_t core_num){
         // LK;printf("core: %d running\n", id);ULK;
 
         // Run
+        uint8_t role = ACCESS;
         while(!(prog1_access_done && prog0_execute_done)) {
             int consume_threshold = prog0_produce_cnt - prog0_consume_cnt;
             int produce_threshold = FIFO_SIZE - (prog1_produce_cnt - prog1_consume_cnt);
-            if (((consume_threshold > ((FIFO_SIZE * 3)/4)) || prog1_access_done)) {
+            if (((role == EXECUTE) || prog1_access_done) && (!prog0_execute_done)) {
                 // LK;printf("exec: %d\n", exec_id);ULK;
                 prog0_execute_kernel(exec_id, consume_threshold);
+                role = ACCESS;
             } else {
                 // LK;printf("access: %d\n", id);ULK;
                 prog1_access_kernel(id, produce_threshold);
+                role = EXECUTE;
             }
         }
         // LK;printf("core: %d waiting\n", id);ULK;
@@ -447,7 +383,7 @@ int main(int argc, char ** argv) {
     id = argv[0][0];
     core_num = argv[0][1];
     if (id == 0) init_tile(NUM * 2);
-    // LK;printf("ID: %d of %d\n", id, core_num);ULK
+    LK;printf("ID: %d of %d\n", id, core_num);ULK
     ATOMIC_OP(amo_cnt, 1, add, w);
     while(core_num != amo_cnt);
     _kernel_(id,core_num);
